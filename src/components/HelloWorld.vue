82c260f198a4db5ea78ee126f7e8c009f0ea0c03
liukl <1138493417@qq.com>
2022-07-18 10:22:12 +0800

    feat:init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+import { app } from '@tauri-apps/api'
+defineProps({
+  msg: String
+})
+
+const i = ref(0)
+let name = $ref('')
+let version = $ref('')
+let tauriVersion = $ref('')
+;(async () => {
+  name = await app.getName()
+  version = await app.getVersion()
+  tauriVersion = await app.getTauriVersion()
+})()
+</script>
+
+<template>
+  <h1 style="color: var(--el-color-primary)">
+    <i-ep-element-plus /> Hello Element Plus!
+  </h1>
+
+  <el-button @click="i++">Incrase: {{ i }}</el-button>
+
+  <el-descriptions title="App">
+    <el-descriptions-item label="Name">
+      {{ name }}
+    </el-descriptions-item>
+    <el-descriptions-item label="Version">
+      {{ version }}
+    </el-descriptions-item>
+    <el-descriptions-item label="Tauri Version">
+      {{ tauriVersion }}
+    </el-descriptions-item>
+  </el-descriptions>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
